17026c6e38942f3e3c453497d219c19fb20dad95
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-08-06 06:53:25 +0700

    feat(Filament): Create CreatorInfo widget and view

---
+++ app/Filament/Widgets/CreatorInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class CreatorInfo extends Widget
+{
+    protected static ?int $sort = 1;
+
+    protected static string $view = 'filament.widgets.creator-info';
+}
